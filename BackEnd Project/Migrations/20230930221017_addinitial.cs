7a27ea882f35eabee22efa557119c2beede44d88
umud <umidhg@code.edu.az>
2023-11-02 00:51:25 +0400

    .

---
+++ BackEnd Project/Migrations/20230930221017_addinitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd_Project.Migrations
+{
+    public partial class addinitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
